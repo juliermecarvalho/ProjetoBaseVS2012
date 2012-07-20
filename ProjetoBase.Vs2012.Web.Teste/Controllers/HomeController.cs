d8fea9ac251b28cc808cc204e1149e2c1c4bcc49
julierme carvalho de oliveira <juliermecarvalho@gmail.com>
2012-07-19 22:20:21 -0300

    commit

--- ProjetoBase.Vs2012.Web.Teste/Controllers/HomeController.cs
+++ ProjetoBase.Vs2012.Web.Teste/Controllers/HomeController.cs
@@ -15,18 +15,5 @@ namespace ProjetoBase.Vs2012.Web.Teste.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
