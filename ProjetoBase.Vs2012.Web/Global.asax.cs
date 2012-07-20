1bd7ab1991e7462453c8ee35b181ec1fa194b0e1
julierme carvalho de oliveira <juliermecarvalho@gmail.com>
2012-07-19 22:23:43 -0300

    commit

--- ProjetoBase.Vs2012.Web/Global.asax.cs
+++ ProjetoBase.Vs2012.Web/Global.asax.cs
@@ -23,7 +23,7 @@ namespace ProjetoBase.Vs2012.Web
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
